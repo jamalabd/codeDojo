300472f39e80910bbd425ee7cc85e33781395450
Jamal Abdul-Majid <jamalmajid@Jamals-MBP.attlocal.net>
2023-06-27 15:12:26 -0400

    grassHopper  py

---
+++ codeChallange/codewars/py/grassHopper.py
@@ -0,0 +1,7 @@
+# Messi is a soccer player with goals in three leagues:
+# LaLiga, Copa del Rey, Champions
+# Complete the function to
+# return his total number of goals in all three leagues.
+
+def goals(laLiga, copaDelRey, championsLeague):
+    return laLiga + copaDelRey + championsLeague
